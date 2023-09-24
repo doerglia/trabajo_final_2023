859a6173a72acd1909f685737ad2a431b0c051f6
doerglia <llammoglia@gmail.com>
2023-09-24 11:44:06 -0300

    Repositorio borrador del trabajo final de pythoncba

---
+++ js/validacion.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contact-form");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
+        const mensaje = document.getElementById("mensaje").value;
+
+        if (!nombre || !email || !mensaje) {
+            alert("Por favor, complete todos los campos.");
+        } else {
+            // Envío del formulario al servidor o servicio externo de correo
+            alert("Formulario enviado con éxito.");
+            form.reset();
+        }
+    });
+});
+
