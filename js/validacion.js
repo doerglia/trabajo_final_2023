af50f9f189171c9ad435d68b59bc8afe959fed07
doerglia <llammoglia@gmail.com>
2023-11-20 22:54:20 -0300

    Correccion TPO: se quitan required, añaden iconos redes sociales,y añaden media querys

--- js/validacion.js
+++ js/validacion.js
@@ -1,28 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("contact-form");
+function validarFormulario(event) {
+    event.preventDefault();
 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const nombre = document.getElementById("nombre").value;
-        const email = document.getElementById("email").value;
-        const mensaje = document.getElementById("mensaje").value;
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const mensaje = document.getElementById("mensaje").value;
 
-        if (!nombre || !email || !mensaje) {
-            alert("Por favor, complete todos los campos.");
-        } else {
-            // Envío del formulario al servidor o servicio externo de correo
-            alert("Formulario enviado con éxito.");
-            form.reset();
-        }
-    });
-});
-
-function showDescription(descriptionId) {
-    const descriptionPopup = document.getElementById(descriptionId);
-    descriptionPopup.style.display = "block";
-}
-
-function hideDescription(descriptionId) {
-    const descriptionPopup = document.getElementById(descriptionId);
-    descriptionPopup.style.display = "none";
+    if (!nombre || !email || !mensaje) {
+        alert("Por favor, complete todos los campos.");
+    } else {
+        // Envío del formulario al servidor o servicio externo de correo
+        alert("Formulario enviado con éxito.");
+        document.getElementById("contact-form").reset();
+    }
 }
